324bd19023d62a291536ec09f1a16179e6b2f64a
scroogy-dev <scroogy-dev@users.noreply.github.com>
2026-04-25 11:54:28 +0000

    deploy: ce22fa48e07f6220449911547c8e332a2dde6c21

---
+++ slides/assets/slidev/default-BYBZoewM.js
@@ -0,0 +1,1 @@
+import{o,e as t,i as s}from"../modules/vue-tZS_UvUR.js";import{_ as r}from"../index-lrW0M6Zb.js";const c={},a={class:"slidev-layout default"};function n(e,l){return o(),t("div",a,[s(e.$slots,"default")])}const f=r(c,[["render",n]]);export{f as I};
