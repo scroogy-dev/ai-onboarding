74072d4589fd4f1854649d9ebf96af2d95ce84eb
scroogy-dev <scroogy-dev@users.noreply.github.com>
2026-08-23 12:54:58 +0000

    deploy: 0515ca3943806955b007a5dd76ec4597428970fb

--- slides/assets/slidev/center-DkNLwdeu.js
+++
@@ -1,1 +0,0 @@
-import{o as t,e as o,g as s,i as c}from"../modules/vue-tZS_UvUR.js";import{_ as r}from"../index-B0S9vviM.js";const a={},n={class:"slidev-layout center h-full grid place-content-center"},l={class:"my-auto"};function d(e,_){return t(),o("div",n,[s("div",l,[c(e.$slots,"default")])])}const m=r(a,[["render",d]]);export{m as I};
